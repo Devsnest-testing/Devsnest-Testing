a39f7a44c7795f1c52326031459c3b9ed3f11ae6
Hemant <hrsmart2150@gmail.com>
2022-08-21 01:49:50 +0530

    wrote our first test case

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const port = 8080;
 const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use('/user', require('./routes/users'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
