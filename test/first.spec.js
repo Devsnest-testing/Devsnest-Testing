c8e4847647eaa6f743f10fca87c38389feeb0101
Hemant <hrsmart2150@gmail.com>
2022-08-21 02:00:11 +0530

    wrote our second test case

--- test/first.spec.js
+++ test/first.spec.js
@@ -1,9 +1,13 @@
 var assert = require('assert');
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
+describe('first test case', function () {
     it('should return -1 when the value is not present', function () {
-      assert.strictEqual([1, 2, 3].indexOf(3), 2);
+        assert.strictEqual([1, 2, 3].indexOf(3), 2);
     });
-  });
 });
+
+describe('second test case', function () {
+    it('should return true', function () {
+        assert.equal(true, true);
+    })
+})
