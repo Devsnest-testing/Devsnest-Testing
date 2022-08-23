cbe58515c2a4d14c57dc0d9fbcb4eaea4ab4967d
Hemant <hrsmart2150@gmail.com>
2022-08-23 23:04:20 +0530

    Copied site testcases with html

--- src/tests/app.spec.js
+++ src/tests/app.spec.js
@@ -9,6 +9,7 @@ const test = document.getElementById('test');
 const leftComm = document.getElementById('leftComm');
 const rightComm = document.getElementById('rightComm');
 
+
 test.addEventListener('click', () => {
     // mocha.run(() => {
     //     mocha.run(() => {
@@ -22,24 +23,24 @@ test.addEventListener('click', () => {
 })
 
 describe("Tesing questions", function () {
-    it('Heading should be devsnest', () => {
-        // console.log(heading.textContent)
-        // heading.innerText = 'Devsnest';
-        expect(heading.value).to.equal('Devsnest');
-    })
+    // it('Heading should be devsnest', () => {
+    //     // console.log(heading.textContent)
+    //     // heading.innerText = 'Devsnest';
+    //     expect(heading.value).to.equal('Devsnest');
+    // })
 
-    it('Heading tag should be closed', () => {
-        expect(closeHeading.value).to.equal('</h1>');
-    })
+    // it('Heading tag should be closed', () => {
+    //     expect(closeHeading.value).to.equal('</h1>');
+    // })
 
-    it('Set the para color to blue', () => {
-        expect(paraBlue.value).to.equal('style="color:blue;"');
-    })
+    // it('Set the para color to blue', () => {
+    //     expect(paraBlue.value).to.equal('style="color:blue;"');
+    // })
 
-    it('Checking comments syntax', () => {
-        expect(leftComm.value).to.equal('<!--');
-        expect(rightComm.value).to.equal('-->');
-    })
+    // it('Checking comments syntax', () => {
+    //     expect(leftComm.value).to.equal('<!--');
+    //     expect(rightComm.value).to.equal('-->');
+    // })
 
 });
 
