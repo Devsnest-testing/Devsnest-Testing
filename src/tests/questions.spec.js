7d83fcfb486731d00a384707b0c9795216083e60
Hemant <hrsmart2150@gmail.com>
2022-08-25 18:07:07 +0530

    fomatted files

--- src/tests/questions.spec.js
+++ src/tests/questions.spec.js
@@ -1,3 +1,11 @@
+const { chai, mocha } = window;
+const { assert, expect } = chai;
+
+test.addEventListener('click', () => {
+    mocha.run();
+})
+
+
 const brTag = document.querySelector('br')
 const spanTag = document.querySelector('span')
 const inputTag = document.querySelector('input')
@@ -24,119 +32,121 @@ const formTagBlank = document.querySelector('form[target="_blank"]')
 const formTagInput = document.getElementsByTagName('form')[0].getElementsByTagName('input')
 const formTagButton = document.getElementsByTagName('form')[0].getElementsByTagName('button')
 
+describe("testing huehue", () => {
+    // Checking <br> tag 
+    it('<br> should exists', async () => {
+        assert.exists(brTag)
+    })
 
-// Checking <br> tag 
-it('<br> should exists', async () => {
-    assert.exists(brTag)
-})
+    // Checking <span> tag 
+    it('<span> should exists', async () => {
+        assert.exists(spanTag)
+    })
 
-// Checking <span> tag 
-it('<span> should exists', async () => {
-    assert.exists(spanTag)
-})
+    // Checking <input> tag 
+    it('<input> should exists', async () => {
+        assert.exists(inputTag)
+    })
 
-// Checking <input> tag 
-it('<input> should exists', async () => {
-    assert.exists(inputTag)
-})
+    // Checking <input> tag of type "text"
+    it('<input> of type "text" should exists', async () => {
+        assert.exists(inputTagText)
+    })
 
-// Checking <input> tag of type "text"
-it('<input> of type "text" should exists', async () => {
-    assert.exists(inputTagText)
-})
+    // Checking <input> tag of type "number"
+    it('<input> of type "number" should exists', async () => {
+        assert.exists(inputTagNumber)
+    })
 
-// Checking <input> tag of type "number"
-it('<input> of type "number" should exists', async () => {
-    assert.exists(inputTagNumber)
-})
+    // Checking <input> tag of type "submit"
+    it('<input> of type "submit" should exists', async () => {
+        assert.exists(inputTagSubmit)
+    })
 
-// Checking <input> tag of type "submit"
-it('<input> of type "submit" should exists', async () => {
-    assert.exists(inputTagSubmit)
-})
+    // Checking <label> tag
+    it('<label> should exists', async () => {
+        assert.exists(labelTag)
+    })
 
-// Checking <label> tag
-it('<label> should exists', async () => {
-    assert.exists(labelTag)
-})
+    // Checking <ul> tag
+    it('<ul> should exists', async () => {
+        assert.exists(ulTag)
+    })
 
-// Checking <ul> tag
-it('<ul> should exists', async () => {
-    assert.exists(ulTag)
-})
-
-// Checking <ul> and <li> tag
-it('<li> should exists of text "Coffee" and "Tea"', async () => {
-    // console.log(liTag[0].innerText)
-    // console.log(liTag[1].innerText)
-    assert.equal(liTag[0].innerText, 'Coffee')
-    assert.equal(liTag[1].innerText, 'Tea')
-})
+    // Checking <ul> and <li> tag
+    it('<li> should exists of text "Coffee" and "Tea"', async () => {
+        // console.log(liTag[0].innerText)
+        // console.log(liTag[1].innerText)
+        assert.equal(liTag[0].innerText, 'Coffee')
+        assert.equal(liTag[1].innerText, 'Tea')
+    })
 
 
-// Checking <header> tag
-it('<header> tag should exists', async () => {
-    assert.exists(headerTag)
-})
+    // Checking <header> tag
+    it('<header> tag should exists', async () => {
+        assert.exists(headerTag)
+    })
 
-// Checking <nav> tag
-it('<nav> tag should exists', async () => {
-    assert.exists(navTag)
-})
+    // Checking <nav> tag
+    it('<nav> tag should exists', async () => {
+        assert.exists(navTag)
+    })
 
-// Checking <main> tag
-it('<main> tag should exists', async () => {
-    assert.exists(mainTag)
-})
+    // Checking <main> tag
+    it('<main> tag should exists', async () => {
+        assert.exists(mainTag)
+    })
 
-// Checking <footer> tag
-it('<footer> tag should exists', async () => {
-    assert.exists(footerTag)
-})
+    // Checking <footer> tag
+    it('<footer> tag should exists', async () => {
+        assert.exists(footerTag)
+    })
 
-// Checking <button> tag 
-it('<button> should exists', async () => {
-    assert.exists(buttonTag)
-})
+    // Checking <button> tag 
+    it('<button> should exists', async () => {
+        assert.exists(buttonTag)
+    })
 
-// Checking <button> tag of type "reset"
-it('<button> of type "reset" should exists', async () => {
-    assert.exists(buttonTagReset)
-})
+    // Checking <button> tag of type "reset"
+    it('<button> of type "reset" should exists', async () => {
+        assert.exists(buttonTagReset)
+    })
 
-// Checking <button> tag of type "submit"
-it('<button> of type "submit" should exists', async () => {
-    assert.exists(buttonTagSubmit)
-})
+    // Checking <button> tag of type "submit"
+    it('<button> of type "submit" should exists', async () => {
+        assert.exists(buttonTagSubmit)
+    })
 
-// Checking table of 2 rows and 3 columns
-it('<li> should exists of text "Coffee" and "Tea"', async () => {
-    console.log(trTag.length)
-    assert.exists(tableTag)
-    // assert.equal(trTag)
-    // assert.equal(thTag)
-    // assert.equal(tdTag)
-})
+    // Checking table of 2 rows and 3 columns
+    it('<li> should exists of text "Coffee" and "Tea"', async () => {
+        console.log(trTag.length)
+        assert.exists(tableTag)
+        // assert.equal(trTag)
+        // assert.equal(thTag)
+        // assert.equal(tdTag)
+    })
 
-// Checking <form> tag of method "GET"
-it('<form> of method "GET" should exists', async () => {
-    assert.exists(formTagGet)
-})
+    // Checking <form> tag of method "GET"
+    it('<form> of method "GET" should exists', async () => {
+        assert.exists(formTagGet)
+    })
 
-// Checking <form> tag of method "POST"
-it('<form> of method "POST" should exists', async () => {
-    assert.exists(formTagPost)
-})
+    // Checking <form> tag of method "POST"
+    it('<form> of method "POST" should exists', async () => {
+        assert.exists(formTagPost)
+    })
 
-// Checking <form> tag of target "_blank"
-it('<form> of target "_blank" should exists', async () => {
-    assert.exists(formTagBlank)
-})
+    // Checking <form> tag of target "_blank"
+    it('<form> of target "_blank" should exists', async () => {
+        assert.exists(formTagBlank)
+    })
 
-// Checking <form> with two input fields
-it('<form> tag with two input fields and one button should exists', async () => {
-    // console.log(formTagInput.length+"😌😌")
-    // console.log(formTagButton)
-    assert.equal(formTagInput.length, 2)
-    assert.equal(formTagButton.length, 1)
+    // Checking <form> with two input fields
+    it('<form> tag with two input fields and one button should exists', async () => {
+        // console.log(formTagInput.length+"😌😌")
+        // console.log(formTagButton)
+        assert.equal(formTagInput.length, 2)
+        assert.equal(formTagButton.length, 1)
+    })
+
 })
